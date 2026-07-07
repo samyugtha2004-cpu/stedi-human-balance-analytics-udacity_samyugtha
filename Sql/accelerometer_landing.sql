eb84c2a7dbebf6d95efe267d5013882b1488e7fb
samyugtha2004-cpu <samyugtha2004@gmail.com>
2026-07-07 15:21:05 +0530

    Create accelerometer_landing.sql

---
+++ Sql/accelerometer_landing.sql
@@ -0,0 +1,28 @@
+CREATE EXTERNAL TABLE `accelerometer`(
+  `user` string COMMENT 'from deserializer', 
+  `timestamp` bigint COMMENT 'from deserializer', 
+  `x` double COMMENT 'from deserializer', 
+  `y` double COMMENT 'from deserializer', 
+  `z` double COMMENT 'from deserializer')
+ROW FORMAT SERDE 
+  'org.openx.data.jsonserde.JsonSerDe' 
+WITH SERDEPROPERTIES ( 
+  'paths'='timestamp,user,x,y,z') 
+STORED AS INPUTFORMAT 
+  'org.apache.hadoop.mapred.TextInputFormat' 
+OUTPUTFORMAT 
+  'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION
+  's3://stedi-samyugtha-01/accelerometer/'
+TBLPROPERTIES (
+  'CRAWL_RUN_ID'='c9a5b1fb-77f0-4ab0-8c8d-7f8947b16af4', 
+  'CrawlerSchemaDeserializerVersion'='1.0', 
+  'CrawlerSchemaSerializerVersion'='1.0', 
+  'UPDATED_BY_CRAWLER'='accelerometer_landing_crawler', 
+  'averageRecordSize'='761', 
+  'classification'='json', 
+  'compressionType'='none', 
+  'objectCount'='9', 
+  'recordCount'='9007', 
+  'sizeKey'='6871328', 
+  'typeOfData'='file')
