5b785ed1ddd016b83d5dbb871024d3a8c2efe14c
samyugtha2004-cpu <samyugtha2004@gmail.com>
2026-07-07 15:19:06 +0530

    Create machine_learning_curated.py

---
+++ Glue_jobs/machine_learning_curated.py
@@ -0,0 +1,48 @@
+import sys
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+from awsglue.utils import getResolvedOptions
+from pyspark.sql.functions import col
+
+## @params: [JOB_NAME]
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
+step_df=spark.read.parquet(
+    "s3://stedi-samyugtha-01/step_trainer_trusted/"
+)
+
+acc_df=spark.read.parquet(
+    "s3://stedi-samyugtha-01/accelerometer_trusted/"
+)
+
+acc_df=acc_df.withColumnRenamed(
+    "timestamp",
+    "sensorreadingtime"
+)
+
+machine_learning_curated_df=step_df.join(
+    acc_df,
+    on="sensorreadingtime",
+    how="inner"
+)
+
+machine_learning_curated_df=machine_learning_curated_df.select(
+    step_df["serialnumber"],
+    step_df["sensorreadingtime"],
+    step_df["distancefromobject"],
+    acc_df["x"],
+    acc_df["y"],
+    acc_df["z"]
+)
+machine_learning_curated_df.write.mode("overwrite").parquet(
+    "s3://stedi-samyugtha-01/machine_learning_curated"
+)
+
+job.commit()
