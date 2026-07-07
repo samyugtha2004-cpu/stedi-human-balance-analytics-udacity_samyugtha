45601bfb09dd63eb136866f990eb9e8ae0f5b1ab
samyugtha2004-cpu <samyugtha2004@gmail.com>
2026-07-07 15:21:55 +0530

    Create step_trainer_landing.sql

---
+++ Sql/step_trainer_landing.sql
@@ -0,0 +1,26 @@
+CREATE EXTERNAL TABLE `step_trainer`(
+  `sensorreadingtime` bigint COMMENT 'from deserializer', 
+  `serialnumber` string COMMENT 'from deserializer', 
+  `distancefromobject` int COMMENT 'from deserializer')
+ROW FORMAT SERDE 
+  'org.openx.data.jsonserde.JsonSerDe' 
+WITH SERDEPROPERTIES ( 
+  'paths'='distanceFromObject,sensorReadingTime,serialNumber') 
+STORED AS INPUTFORMAT 
+  'org.apache.hadoop.mapred.TextInputFormat' 
+OUTPUTFORMAT 
+  'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION
+  's3://stedi-samyugtha-01/step_trainer/'
+TBLPROPERTIES (
+  'CRAWL_RUN_ID'='99051920-541c-4991-8a23-5e1f19465868', 
+  'CrawlerSchemaDeserializerVersion'='1.0', 
+  'CrawlerSchemaSerializerVersion'='1.0', 
+  'UPDATED_BY_CRAWLER'='step_trainer_landing_crawler', 
+  'averageRecordSize'='1032', 
+  'classification'='json', 
+  'compressionType'='none', 
+  'objectCount'='3', 
+  'recordCount'='3194', 
+  'sizeKey'='3298200', 
+  'typeOfData'='file')
